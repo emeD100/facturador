e7e2b8d24e190125473cf830d2ffd3230dfb3551
Md110e <md110e@gmail.com>
2019-04-06 13:54:09 +0200

    primer commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Settings
+
+// Middlewares
+
+
+// Routes
+
+
+// Starting the server
+app.listen(3000, () => {
+    console.log('Server on port 3000');
+});
